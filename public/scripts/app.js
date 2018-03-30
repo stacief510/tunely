1c43166311f18779c03fc711d9edd0413dc8c15f
stacief510 <staciemfraser@gmail.com>
2018-03-30 11:38:42 -0700

    temp commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,27 +7,27 @@ $(document).ready(function() {
   console.log('app.js loaded!');
 
 // hard-coded data
-var sampleAlbums = [{
-  artistName: 'Ladyhawke',
-  name: 'Ladyhawke',
-  releaseDate: '2008, November 18',
-  genres: [ 'new wave', 'indie rock', 'synth pop' ]
-}, {
-  artistName: 'The Knife',
-  name: 'Silent Shout',
-  releaseDate: '2006, February 17',
-  genres: [ 'synth pop', 'electronica', 'experimental' ]
-}, {
-  artistName: 'Juno Reactor',
-  name: 'Shango',
-  releaseDate: '2000, October 9',
-  genres: [ 'electronic', 'goa trance', 'tribal house' ]
-}, {
-  artistName: 'Philip Wesley',
-  name: 'Dark Night of the Soul',
-  releaseDate: '2008, September 12',
-  genres: [ 'piano' ]
-}];
+// var sampleAlbums = [{
+//   artistName: 'Ladyhawke',
+//   name: 'Ladyhawke',
+//   releaseDate: '2008, November 18',
+//   genres: [ 'new wave', 'indie rock', 'synth pop' ]
+// }, {
+//   artistName: 'The Knife',
+//   name: 'Silent Shout',
+//   releaseDate: '2006, February 17',
+//   genres: [ 'synth pop', 'electronica', 'experimental' ]
+// }, {
+//   artistName: 'Juno Reactor',
+//   name: 'Shango',
+//   releaseDate: '2000, October 9',
+//   genres: [ 'electronic', 'goa trance', 'tribal house' ]
+// }, {
+//   artistName: 'Philip Wesley',
+//   name: 'Dark Night of the Soul',
+//   releaseDate: '2008, September 12',
+//   genres: [ 'piano' ]
+// }];
 
   $.ajax({
       method: 'GET',
